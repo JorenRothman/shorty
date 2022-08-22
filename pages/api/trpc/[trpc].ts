92afae3a86ab6ba386380ede140208bf362b9b26
Joren Rothman <joren@instance.studio>
2022-08-22 22:30:23 +0200

    Added trpc

---
+++ pages/api/trpc/[trpc].ts
@@ -0,0 +1,47 @@
+import * as trpc from '@trpc/server';
+import * as trpcNext from '@trpc/server/adapters/next';
+import { z } from 'zod';
+
+export const appRouter = trpc
+    .router()
+    .query('hello', {
+        input: z
+            .object({
+                text: z.string().nullish(),
+            })
+            .nullish(),
+        resolve({ input }) {
+            return {
+                greeting: `hello ${input?.text ?? 'world'}`,
+            };
+        },
+    })
+    .query('custom', {
+        input: z
+            .object({
+                text: z.string().nullish(),
+            })
+            .nullish(),
+        resolve({ input }) {
+            return {
+                string: `My custom message ${input?.text ?? ''}`,
+            };
+        },
+    })
+    .mutation('url', {
+        input: z.object({
+            url: z.string(),
+        }),
+        resolve({ input }) {
+            console.log(input);
+        },
+    });
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
+
+// export API handler
+export default trpcNext.createNextApiHandler({
+    router: appRouter,
+    createContext: () => null,
+});
